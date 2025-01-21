d6efe1e8be8a534b6376e799e06bf7d072d8bda7
Nabilabintaakter <nabilabintaakter@gmail.com>
2025-01-21 18:42:55 +0600

    added PATCH endpoint for updating user role to Admin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5f9uk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -76,6 +76,19 @@ async function run() {
     app.get('/users', async(req, res)=>{
         const result = await usersCollection.find().toArray();
         res.send(result);
+    })
+
+    // (Admin) PATCH make a user admin
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          role: "Admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter,updatedDoc)
+      res.send(result)
     })
 
 
