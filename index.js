bb5ea34528955313be5a82e080ff68bc56a3b021
Nabilabintaakter <nabilabintaakter@gmail.com>
2025-01-23 04:09:19 +0600

    added verify Admin middleware for verification of Admin

--- index.js
+++ index.js
@@ -53,19 +53,33 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
       res.send({ token })
     })
+    // ***************Middlewares******************
+    // verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === 'Admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+    // verify admin after verifyToken
+    const verifyTeacher = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isTeacher = user?.role === 'Teacher';
+      if (!isTeacher) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
 
-    // POST teacher request by user
-    app.post('/teacher-requests',verifyToken,async (req, res) => {
-      const teacherInfo = req.body;
-      const result = await teachersCollection.insertOne(teacherInfo);
-      res.send(result);
-    })
-    // (Teacher) POST class request by teacher
-    app.post('/classes',verifyToken, async (req, res) => {
-      const classInfo = req.body;
-      const result = await classesCollection.insertOne(classInfo);
-      res.send(result);
-    })
+
+                    // **************USERS related api****************
+
     // POST users data in db
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -78,44 +92,20 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
-    // (Teacher) GET all class requests added by a specific teacher
-    app.get('/classes/:email',verifyToken, async (req, res) => {
-      const email = req.params.email;
-      const query = { email }
-      const result = await classesCollection.find(query).toArray();
-      res.send(result);
-    })
-    // (Admin) GET all teacher requests from users
-    app.get('/teachers',verifyToken, async (req, res) => {
-      const result = await teachersCollection.find().toArray();
-      res.send(result);
-    })
-    // (Admin) GET all class requests from teachers
-    app.get('/classes',verifyToken, async (req, res) => {
-      const result = await classesCollection.find().toArray();
-      res.send(result);
-    })
-    //  GET all class approved by admin
-    app.get('/all-classes', async (req, res) => {
-      const query = { status: 'Accepted' }
-      const result = await classesCollection.find(query).toArray();
-      res.send(result);
-    })
-    //  GET a class details by id
-    app.get('/class/:id',verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await classesCollection.findOne(query);
-      res.send(result);
+    // get user role (for all users either logged in or not)
+    app.get('/users/role/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send({ role: result?.role })
     })
 
     // (Admin) GET all users info
-    app.get('/users',verifyToken, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
-    // GET a user his/her info by email
-    app.get('/users/:email',verifyToken, async (req, res) => {
+    // GET a user his/her info by email 
+    app.get('/users/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email }
       const result = await usersCollection.find(query).toArray();
@@ -123,7 +113,7 @@ async function run() {
     })
 
     // (Admin) PATCH make a user admin
-    app.patch('/users/admin/:id',verifyToken, async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
@@ -135,8 +125,31 @@ async function run() {
       res.send(result)
     })
 
+
+                    // **************TEACHER & CLASS related api***************
+
+    // POST a teacher request by user
+    app.post('/teacher-requests', verifyToken, async (req, res) => {
+      const teacherInfo = req.body;
+      const result = await teachersCollection.insertOne(teacherInfo);
+      res.send(result);
+    })
+    // (Teacher) POST a class request by teacher
+    app.post('/classes', verifyToken, verifyTeacher, async (req, res) => {
+      const classInfo = req.body;
+      const result = await classesCollection.insertOne(classInfo);
+      res.send(result);
+    })
+
+    // (Teacher) GET all class requests added by a specific teacher
+    app.get('/classes/:email', verifyToken, verifyTeacher, async (req, res) => {
+      const email = req.params.email;
+      const query = { email }
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    })
     // (Admin) PATCH approve a teacher
-    app.patch('/users/teacher-approve/:email',verifyToken, async (req, res) => {
+    app.patch('/users/teacher-approve/:email', verifyToken, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       if (!email) {
         return res.status(400).send({ error: "Email is required" });
@@ -165,7 +178,7 @@ async function run() {
       }
     });
     // (Admin) PATCH reject a teacher
-    app.patch('/users/teacher-reject/:email',verifyToken, async (req, res) => {
+    app.patch('/users/teacher-reject/:email', verifyToken, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       if (!email) {
         return res.status(400).send({ error: "Email is required" });
@@ -193,9 +206,8 @@ async function run() {
         res.status(500).send({ error: "Failed to update user role" });
       }
     });
-
     // (Admin) PATCH approve a class
-    app.patch('/admin/approve-class/:id',verifyToken, async (req, res) => {
+    app.patch('/admin/approve-class/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       if (!id) {
         return res.status(400).send({ error: "Something went wrong." });
@@ -215,7 +227,7 @@ async function run() {
       }
     });
     // (Admin) PATCH reject a class
-    app.patch('/admin/reject-class/:id',verifyToken, async (req, res) => {
+    app.patch('/admin/reject-class/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       if (!id) {
         return res.status(400).send({ error: "Something went wrong." });
@@ -234,6 +246,31 @@ async function run() {
         res.status(500).send({ error: "Failed to update class status" });
       }
     });
+    // (Admin) GET all teacher requests from users
+    app.get('/teachers', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await teachersCollection.find().toArray();
+      res.send(result);
+    })
+    // (Admin) GET all class requests from teachers
+    app.get('/classes', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    })
+    //  GET all class approved by admin (for all users either logged in or not)
+    app.get('/all-classes', async (req, res) => {
+      const query = { status: 'Accepted' }
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    })
+    //  GET a class details by id
+    app.get('/class/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.findOne(query);
+      res.send(result);
+    })
+
+
 
 
 
